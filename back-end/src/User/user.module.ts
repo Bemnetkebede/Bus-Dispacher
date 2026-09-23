e78f1649897368f63676e3e8fb4107411c04fb6a
BemnetKebede <bemnet026@gmail.com>
2026-09-23 19:41:59 +0300

    fix: resolve circular dependency between User and Auth modules

--- back-end/src/User/user.module.ts
+++ back-end/src/User/user.module.ts
@@ -1,13 +1,13 @@
-import  { Module } from '@nestjs/common';
-import { UserService } from './user.service.js';
-import { PrismaModule } from '../../prisma/prisma.module.js'
-import { AuthModule } from '../auth/auth.module.js';
+import { Module, forwardRef } from '@nestjs/common';
 import { UserController } from './user.controller.js';
+import { UserService } from './user.service.js';
+import { AuthModule } from '../auth/auth.module.js'; 
+import { PrismaModule } from '../../prisma/prisma.module.js'; 
 
 @Module({
-    imports: [PrismaModule, AuthModule],
-    controllers: [UserController],
-    providers: [UserService],
-    exports: [UserService],
+  // Wrap AuthModule with forwardRef
+  imports: [PrismaModule, forwardRef(() => AuthModule)], 
+  controllers: [UserController],
+  providers: [UserService]
 })
 export class UserModule {}
