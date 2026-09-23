e1e19a9ae42d8ffc641ad7a3c03bd4cf44a786c1
BemnetKebede <bemnet026@gmail.com>
2026-09-23 21:59:02 +0300

    fix: remove direct UserModule import to resolve circular dependency

--- back-end/src/bus/bus.module.ts
+++ back-end/src/bus/bus.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { BusService } from './bus.service.js';
 import { BusController } from './bus.controller.js';
+import {AuthModule} from '../auth/auth.module.js'
 
 @Module({
+  imports : [AuthModule],
   controllers: [BusController],
   providers: [BusService],
 })
