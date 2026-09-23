0787156bff84ce8b8d497c4c3613c2886117e9d4
BemnetKebede <bemnet026@gmail.com>
2026-09-23 19:32:37 +0300

    fix: move AuthModule to imports in UserModule

--- back-end/src/User/user.module.ts
+++ back-end/src/User/user.module.ts
@@ -1,10 +1,12 @@
 import  { Module } from '@nestjs/common';
 import { UserService } from './user.service.js';
+import { PrismaModule } from '../../prisma/prisma.module.js'
 import { AuthModule } from '../auth/auth.module.js';
 import { UserController } from './user.controller.js';
 
 @Module({
-    controllers : [UserController , AuthModule],
+    imports: [PrismaModule, AuthModule],
+    controllers: [UserController],
     providers: [UserService],
     exports: [UserService],
 })
